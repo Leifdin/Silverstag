8ec9ce47bac8692e7c33faba3dbab572b9252d12
Leifdin <pavolpolonec@gmail.com>
2016-08-22 09:58:27 +0200

    Some item tweaks
    Signed-off-by: Leifdin <pavolpolonec@gmail.com>

--- Main Source/module_party_templates.py
+++ Main Source/module_party_templates.py
@@ -251,6 +251,6 @@ party_templates = [
   ("leaded_looters","Band of robbers",icon_axeman|carries_goods(8)|pf_quest_party,0,fac_neutral,bandit_personality,[(trp_looter_leader,1,1),(trp_looter,3,3)]),
   
   ("troop_testing_party_1", "Troop Testing Party", icon_axeman|pf_is_static, 0, fac_outlaws, troop_testers, [(trp_new_nord_berserker, 50, 50)]),
-  ("troop_testing_party_2", "Troop Testing Party", icon_axeman|pf_is_static, 0, fac_outlaws, troop_testers, [(trp_new_rhodok_halberdier, 50, 50)]),
+  ("troop_testing_party_2", "Troop Testing Party", icon_axeman|pf_is_static, 0, fac_outlaws, troop_testers, [(trp_new_rhodok_footman, 50, 50)]),
   ("troop_testing_party_3", "Troop Testing Party", icon_axeman|pf_is_static, 0, fac_outlaws, troop_testers, [(trp_looter, 1, 1)]),
 ]
