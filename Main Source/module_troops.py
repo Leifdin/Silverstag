356e6f69208c8ffb948582c6e65afcf1d82cfbc1
pavolpolonec@gmail.com <pavolpolonec@gmail.com>
2017-07-24 21:14:40 +0200

    Nordic Recruit now upgrades to Nordic Tracker (Iss 22)
    Signed-off-by: pavolpolonec@gmail.com <pavolpolonec@gmail.com>

--- Main Source/module_troops.py
+++ Main Source/module_troops.py
@@ -2278,6 +2278,6 @@ upgrade(troops,"fighter_woman","sword_sister")
 upgrade2(troops, "n_swadian_recruit", "n_swadian_militia", "n_swadian_crossbowman"),
 upgrade2(troops, "n_vaegir_recruit", "n_vaegir_skirmisher", "n_vaegir_raider"),
 upgrade2(troops, "n_khergit_recruit", "n_khergit_scout", "n_khergit_shaman"),
-upgrade2(troops, "n_nordic_recruit", "n_nordic_skald", "n_nordic_retinue_archer"),
+upgrade2(troops, "n_nordic_recruit", "n_nordic_skald", "n_nordic_tracker"),
 upgrade2(troops, "n_rhodok_recruit", "n_rhodok_ranger", "n_rhodok_footman"),
 
